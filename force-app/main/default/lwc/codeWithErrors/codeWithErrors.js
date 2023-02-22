cf1a6e4dabd8f37ac5a21046ea58d951ecbb8045
Fabien Taillon <ftaillon@gmail.com>
2023-02-22 14:59:10 +0100

    Init

---
+++ force-app/main/default/lwc/codeWithErrors/codeWithErrors.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class CodeWithErrors extends LightningElement {
+
+    handleError() {
+        const my = new Date();
+        const endDateTime = startDateTime.setMinutes(startDateTime.getMinutes() + CALL_DURATION_IN_MINUTES);
+    }
+}
